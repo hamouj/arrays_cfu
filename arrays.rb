121a75abf77a38b61d71ed0ae8de4751578e1862
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-10-16 20:15:36 -0700

    Add methods and definitions

--- arrays.rb
+++ arrays.rb
@@ -1,8 +1,19 @@
-favorite_starbucks_items = ["Americano", "Egg Sandwich", "Croissan", "Cake Pop"]
+favorite_starbucks_items = ["Americano", "Egg Sandwich", "Croissant", "Cake Pop"]
 
 birth_months = [2, 4, 5, 6]
 
-cost_starbucks_items = [3.45, 4.75, 3.45, 2.75]
+cost_starbucks_items = [3.45, 4.75, 3.75, 2.75]
 
 true_statements = [true, true, true, true]
 
+# Pop removes the last element from the array and gives it as the output.
+puts cost_starbucks_items.pop
+
+# Shift removes the first element from the array and gives it as the output.
+puts birth_months.shift
+
+# Push inserts an element(s) into the end of the array.
+puts true_statements.push [false, false]
+
+# Unshift inserts an element(s) into the beginning of the array.
+puts favorite_starbucks_items.unshift ["water"]
