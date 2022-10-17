6917fc0483f20d1c46049edbf5568126d5533d53
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-10-16 20:04:13 -0700

    Declare variables for arrays

---
+++ arrays.rb
@@ -0,0 +1,8 @@
+favorite_starbucks_items = ["Americano", "Egg Sandwich", "Croissan", "Cake Pop"]
+
+birth_months = [2, 4, 5, 6]
+
+cost_starbucks_items = [3.45, 4.75, 3.45, 2.75]
+
+true_statements = [true, true, true, true]
+
