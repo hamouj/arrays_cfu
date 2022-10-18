67c69eb7e0a664d02b29a0f9d07c8cbf5e5321b2
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-10-17 20:23:29 -0700

    Add checks after each method and add technical terminology

--- arrays.rb
+++ arrays.rb
@@ -8,21 +8,25 @@ true_statements = [true, true, true, true]
 
 # Pop removes the last element from the array and gives it as the output.
 puts cost_starbucks_items.pop
+puts cost_starbucks_items
 
 # Shift removes the first element from the array and gives it as the output.
 puts birth_months.shift
+puts birth_months
 
 # Push inserts an element(s) into the end of the array.
 puts true_statements.push [false, false]
+puts true_statements
 
 # Unshift inserts an element(s) into the beginning of the array.
 puts favorite_starbucks_items.unshift ["water"]
+puts favorite_starbucks_items
 
 # -------------------Index Positions----------------------
-# Instead of starting with 1, we start at 0 when counting positions in an array.
+# Instead of starting with 1, we start at 0 when counting positions in an array--this is called index positions.
 # For example, in the variable "birth_months", the 2 is in the index position 0 and the 6 is in the index position 3.
 
 # -------------------Reverse Method--------------------
 # The reverse method reverses the order of the elements within an array.
 puts birth_months.reverse
-# By calling the reverse method on the birth_months array, the output is a reversal of the birth months [6,5,4]
+# By calling the reverse method on the birth_months array, the output is a reversal of the birth month elements within the array [6,5,4]
